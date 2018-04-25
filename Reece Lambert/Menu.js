9ec911b81359e1f75997c91ebd69706ab034b20a
ReeceLambert <reece.lambert@hotmail.com>
2018-04-25 13:14:03 +0100

    Created a button which changes color when cursor moves over it

--- Reece Lambert/Menu.js
+++ Reece Lambert/Menu.js
@@ -1,6 +1,13 @@
 function rlMenu(){
 	this.update = rl_menu_update;
 	this.draw = rl_menu_draw;
+
+	this.click = rl_menu_click;
+    this.mouseMove = rl_menu_mouse_move;
+
+	this.firstButtonHighlighted = false;
+
+	this.button1 = new Button(50, 200, 225, 70, "Start");
 }
 
 function rl_menu_update(){
@@ -8,8 +15,34 @@ function rl_menu_update(){
 }
 
 function rl_menu_draw(ctx){
-ctx.fillRect(20,20,250,100);
+	this.button1.draw(ctx);
+	/*
+	if(this.firstButtonHighlighted){
+		ctx.fillStyle=("#FF0000")
+	}
+	else {
+		ctx.fillStyle=("#800000")
+	}
+	ctx.fillRect(50,200,225,70);
+	ctx.fillStyle=("#FF0000");
+	ctx.fillRect(325,200,225,70);
+	ctx.fillStyle=("#FF0000");
+	ctx.fillRect(50,300,225,70);
+	ctx.fillStyle=("#800000");
+	ctx.fillRect(325,300,225,70);
+	ctx.fillStyle=("#000000");
+	*/
 
-	ctx.font= "30px Arial";
-	ctx.fillText("Impossible Quiz",300,50);
+	ctx.font= "30px 'Gugi";
+	ctx.fillText("The Highly Improbable Quiz",100,50);
+}
+
+function rl_menu_click(x, y){
+  console.log("Clicked at: " + x + ", " + y);
+  return this;
+}
+
+function rl_menu_mouse_move(x, y){
+	this.button1.hitTest(x, y);
+
 }
