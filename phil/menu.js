4cff78e000ec5b9494b6e6911534bdf7d20636fc
Phil Jeffes <philjeffes@hotmail.com>
2018-04-25 13:00:17 +0100

    Added button

--- phil/menu.js
+++ phil/menu.js
@@ -4,6 +4,10 @@ function PJMenu(){
 
     this.click = pj_menu_click;
     this.mouseMove = pj_menu_mouse_move;
+
+    this.firstButtonHighlighted = false;
+
+    this.button1 = new Button(20, 20, 150, 100, "Start");
 }
 
 function pj_menu_update(){
@@ -11,12 +15,10 @@ function pj_menu_update(){
 }
 
 function pj_menu_draw(ctx){
+
   var old_color = ctx.fillStyle;
-  ctx.fillStyle = '#00ff00';
-  ctx.fillRect(20,20,150,100);
-  ctx.fillStyle = '#0000ff';
-  ctx.fillRect(180,20,150,100);
-  ctx.fillStyle = old_color;
+
+  this.button1.draw(ctx);
 
   ctx.font = "30px Arial";
   ctx.fillText("Impossible Quiz",200,50);
@@ -29,5 +31,10 @@ function pj_menu_click(x, y){
 }
 
 function pj_menu_mouse_move(x, y){
-
+  this.button1.hitTest(x, y);
+  // if(x > 20 && x < 170 && y > 20 && y < 120){
+  //   this.firstButtonHighlighted = true;
+  // }else{
+  //   this.firstButtonHighlighted = false;
+  // }
 }
