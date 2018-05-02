0c4627ca4fd0710ec9e954e3f6e51fdbb610a68b
Liam <liamwatson11996@gmail.com>
2018-05-02 11:02:33 +0100

    Added images folder

--- Liam/level2.js
+++ Liam/level2.js
@@ -6,14 +6,16 @@ function LWlevel2(){
   this.mouseMove = lw_level2_mouse_move;
 
   this.firstButtonHighlighted = false;
-  this.button1 = new Button(100, 160, 150, 100, "Beholder");
+  this.button1 = new Button(100, 290, 150, 100, "Beholder");
   this.button1.color = '#f28f04'
   this.button1.colorHighlighted = '#ffbc70'
   this.button1.textColor = '#000000'
-  this.button2 = new Button(325, 160, 150, 100, "Floaty Eyeball");
+  this.button2 = new Button(325, 290, 150, 100, "Floaty Eyeball");
   this.button2.color = '#f28f04'
   this.button2.colorHighlighted = '#ffbc70'
   this.button2.textColor = '#000000'
+
+
 }
 
 function lw_level2_update(){
--- Liam/menu.js
+++ Liam/menu.js
@@ -6,7 +6,7 @@ function LWMenu(){
   this.mouseMove = lw_menu_mouse_move;
 
   this.firstButtonHighlighted = false;
-  this.button1 = new Button(125, 160, 150, 100, "Start");
+  this.button1 = new Button(250, 160, 200, 150, "Start");
   this.button1.color = '#f28f04'
   this.button1.colorHighlighted = '#ffbc70'
   this.button1.textColor = '#000000'
@@ -22,12 +22,12 @@ function lw_menu_draw(ctx){
   this.button1.draw(ctx)
 
   ctx.fillStyle = '#f28f04'
-  ctx.fillRect(325,160,150,100);
+  ctx.fillRect(495,160,200,150);
   ctx.fillStyle = '#f28f04'
-  ctx.fillRect(125,275,350,100)
+  ctx.fillRect(250,375,450,150)
   ctx.fillStyle = '#000000'
   ctx.font = "30px Arial";
-  ctx.fillText("Impossible Quiz",200,50);
+  ctx.fillText("Impossible Quiz",350,50);
   ctx.fillStyle = '#00000'
 
 }
