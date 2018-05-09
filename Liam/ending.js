dde2c5ee198293a78c3cc444b5831b870441b446
unknown <liamwatson11996@gmail.com>
2018-05-09 13:44:25 +0100

    Added an ending, and background

---
+++ Liam/ending.js
@@ -0,0 +1,37 @@
+function LWending(){
+    this.update = lw_ending_update;
+    this.draw = lw_ending_draw
+  
+    this.click = lw_ending_click;
+    this.mouseMove = lw_ending_mouse_move;
+  
+    this.firstButtonHighlighted = false;
+    this.button1 = new Button(350, 160, 300, 200, "Return to Menu");
+    this.button1.color = '#f28f04'
+    this.button1.colorHighlighted = '#ffbc70'
+    this.button1.textColor = '#000000'
+   
+  }
+  
+  function lw_ending_update(){
+  
+  }
+  
+  function lw_ending_draw(ctx){
+    this.button1.draw(ctx);
+    ctx.font = "30px Arial";
+    ctx.fillText("Thank you for playing!",350,50);
+    ctx.fillStyle = '#00000'
+  }
+  
+  
+  
+  function lw_ending_click(x, y){
+    if (this.button1.hitTest(x, y)){
+      return new LWMenu();
+    }
+    return this;
+  }
+  function lw_ending_mouse_move(x, y){
+    this.button1.hitTest(x, y);
+  }
